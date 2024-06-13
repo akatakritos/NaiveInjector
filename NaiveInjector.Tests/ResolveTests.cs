2ea3ee528f732b8d8cd5853ce34af30dfa7f2854
Matt Burke <git@mattburkedev.com>
2024-06-12 20:59:58 -0500

    add dependency support

--- NaiveInjector.Tests/ResolveTests.cs
+++ NaiveInjector.Tests/ResolveTests.cs
@@ -4,8 +4,17 @@ namespace NaiveInjector.Tests;
 
 public class ResolveTests
 {
+    public class UnregisteredClass
+    {
+    }
+
     public class SimpleClass
     {
+    }
+    
+    public class WithDependency(SimpleClass simpleClass)
+    {
+        public SimpleClass SimpleClass { get; } = simpleClass;
     }
     
 
@@ -19,6 +28,29 @@ public class ResolveTests
         var instance = injector.Resolve<SimpleClass>();
         
         instance.Should().BeOfType<SimpleClass>();
+    }
+    
+    [Fact(DisplayName = "It throws an exception when trying to resolve an unregistered class")]
+    public void UnregisteredTest()
+    {
+        var registry = new NaiveRegistry();
+        var injector = registry.Build();
 
+        injector.Invoking(i => i.Resolve<UnregisteredClass>())
+            .Should().Throw<UnregistedTypeException>();
+    }
+    
+    [Fact(DisplayName = "It can resolve a class with a dependency")]
+    public void WithDependencyTest()
+    {
+        var registry = new NaiveRegistry();
+        registry.Register<SimpleClass>();
+        registry.Register<WithDependency>();
+
+        var injector = registry.Build();
+        var instance = injector.Resolve<WithDependency>();
+        
+        instance.Should().BeOfType<WithDependency>();
+        instance.SimpleClass.Should().BeOfType<SimpleClass>();
     }
 }
--- NaiveInjector/NaiveInjector.cs
+++ NaiveInjector/NaiveInjector.cs
@@ -1,21 +1,58 @@
-﻿namespace NaiveInjector;
+﻿using System.Diagnostics;
+
+namespace NaiveInjector;
 
 public class NaiveRegistry
 {
+    private readonly List<Type> _registeredTypes = new();
     public void Register<T>()
     {
+        _registeredTypes.Add(typeof(T));
     }
     
     public NaiveInjector Build()
     {
-        return new NaiveInjector();
+        return new NaiveInjector(_registeredTypes);
     }
 }
 
 public class NaiveInjector
 {
+    private readonly List<Type> _registeredTypes;
+
+    public NaiveInjector(List<Type> registeredTypes)
+    {
+        _registeredTypes = registeredTypes;
+    }
     public T Resolve<T>()
     {
-        return Activator.CreateInstance<T>();
+        return (T)Resolve(typeof(T));
+    }
+    
+    public object Resolve(Type type)
+    {
+        if (!_registeredTypes.Contains(type))
+        {
+            throw new UnregistedTypeException(type);
+        }
+        
+        var ctor = type.GetConstructors()
+            .OrderByDescending(c => c.GetParameters().Length)
+            .First();
+        
+        var parameters = ctor.GetParameters()
+            .Select(p => Resolve(p.ParameterType))
+            .ToArray();
+        var instance = Activator.CreateInstance(type, parameters);
+        Debug.Assert(instance != null);
+        return instance;
+    }
+}
+
+public class UnregistedTypeException : Exception
+{
+    public UnregistedTypeException(Type type)
+        : base($"Type {type} is not registered")
+    {
     }
 }
